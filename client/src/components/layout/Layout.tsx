d51e1b9ddc0c8e476df25f55f68ced104013332b
keshav lath <keshav.lath3232788@gmail.com>
2026-01-16 03:41:51 +0530

    fix build

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,10 +1,9 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-import { Home, Info, Calendar, MessageSquare } from 'lucide-react';
+import { Outlet } from 'react-router-dom';
 import { Footer } from './Footer.tsx';
 import Navbar from "./Navbar.tsx";
 
 export const Layout = () => {
-    const navigate = useNavigate();
+    
 
     return (
         <div className="min-h-screen bg-zinc-950 text-white flex flex-col">
