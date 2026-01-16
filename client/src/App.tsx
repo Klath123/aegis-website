7b2e61b471a9e81e1bf5b087546fd05803cd5f80
VINAYMADIVAL <vinaymadivalcmp@gmail.com>
2026-01-16 22:02:16 +0530

    Add Members Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
-import { Layout } from './components/layout/Layout.tsx';
-import Home from './pages/Home.tsx';
-import About from './pages/About.tsx';
-import Events from './pages/Events.tsx';
-import Contact from './pages/Contact.tsx';
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./components/layout/Layout.tsx";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+import Events from "./pages/Events.tsx";
+import Contact from "./pages/Contact.tsx";
+import Members from "./pages/Members.tsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="events" element={<Events />} />
+        <Route path="members" element={<Members />} />
         <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
